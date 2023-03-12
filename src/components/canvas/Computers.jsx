0b8355950c028f7f5edff871ca59f623a642e340
SenpaiSumpie <max.lee.mosier@gmail.com>
2023-03-12 16:20:40 -0500

    Hero Section Setup

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from 'react';
 
 const Computers = () => {
   return (
---
+++ src/components/canvas/Synthwave.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Synthwave = () => {
+    return (
+        <div>Synthwave</div>
+    )
+}
+
+export default Synthwave
